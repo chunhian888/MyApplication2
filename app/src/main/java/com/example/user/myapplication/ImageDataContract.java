54744a4d52de307d0203183690d11d03d43124de
chunhian888 <chunhian888@hotmail.com>
2017-12-26 15:30:03 +0800

    ALL

---
+++ app/src/main/java/com/example/user/myapplication/ImageDataContract.java
@@ -0,0 +1,16 @@
+package com.example.user.myapplication;
+
+/**
+ * Created by User on 26/12/2017.
+ */
+
+/**
+ * Created by KweeTeck on 11/1/2017.
+ */
+
+public class ImageDataContract {
+    public static final String TABLE_NAME = "album";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_IMAGE="image";
+}
